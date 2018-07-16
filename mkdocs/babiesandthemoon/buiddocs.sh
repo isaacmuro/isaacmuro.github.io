ee81f00ad1bc5429aaa7282e8c13b97deaecc012
isaacmuro <willezumleben@gmail.com>
2018-07-16 18:28:04 +0300

    Construida la documentacio

---
+++ mkdocs/babiesandthemoon/buiddocs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Script per Crear la documentació estàtica i pujar a Git.
+
+# Variables
+DIR=~/git/isaacmur.github.io
+
+#Building Babies And The Moon site
+echo 'Building Babies.......'
+mkdocs build --clean -d $DIR/docs/babiesandthemoon
+
+echo 'Uploading changes to git'
+git add $DIR/docs/babiesandthemoon/*
+git commit -m "Automated script uploaded web"
+git push origin master
+
+echo '#####################################'
+echo 'Done!'
