4430c8dfa8c45d5ac4a1ba443020d6967a9ae725
isaacmg <img@disroot.org>
2019-10-08 10:08:25 +0200

    slides

---
+++ slides/formacioMates/sketchs/prova.js
@@ -0,0 +1,10 @@
+function setup() {
+let myCanvas = createCanvas(400, 400);
+  myCanvas.parent('myContainer');
+}
+
+function draw() {
+//background(#FFFFFF);
+fill(0,0,0);
+  ellipse(50, 50, 80, 80);
+}
